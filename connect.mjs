6ac4b6dc2a38e19a261ca445c9eb88a7903cc4a6
yehiel <yehiel9523@gmail.com>
2021-08-31 15:23:42 +0300

    some changes

--- connect.mjs
+++ connect.mjs
@@ -1,27 +1,62 @@
-import { MongoClient, ObjectId } from 'mongodb';
-const client = new MongoClient('mongodb://localhost:27017');
-let db;
-client.connect(async() => {
-    db = client.db('store');
-    // const collection = db.collection('Products');
-    // const result = await collection.find({}).toArray();
-    console.log('connect to db');
-    // console.log(result)
-});
+import { ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
 
-export function getProducts() {
-    return db.collection('Products').find({}).toArray();
+mongoose.connect('mongodb://localhost:27017/store', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+const productsSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    price: Number,
+    description: String,
+    category: String,
+    image: String,
+})
+const Product = mongoose.model('Products', productsSchema);
+
+
+// import { MongoClient, ObjectId } from 'mongodb';
+// const client = new MongoClient('mongodb://localhost:27017');
+// let db;
+// client.connect(async() => {
+//     db = client.db('store');
+//     // const collection = db.collection('Products');
+//     // const result = await collection.find({}).toArray();
+//     console.log('connect to db');
+//     // console.log(result)
+// });
+
+export function getProducts(filter = {}) {
+    const qwery = {};
+    if (filter.title) {
+        qwery.title = new RegExp(filter.title, 'i')
+    }
+    return Product.find(qwery)
+
+    // return db.collection('Products').find(qwery).toArray();
+    // return db.collection('Products').find({}).toArray();
 }
 export function getProduct(id) {
-    return db.collection('Products').findOne({ id: parseInt(id) });
+    return Product.findOne({ _id: ObjectId(id) })
+        // return db.collection('Product').findOne({ id: parseInt(id) });
 }
+// export function findBySearch(filter = {}) {
+//     const qwery = {};
+//     if (filter.title) {
+//         qwery.title = new RegExp(filter.title, 'i')
+//     }
+//     return db.collection('Product').find(qwery).toArray();
+// }
 export function addProduct(product) {
-    if (product.id)
-        return db.collection('Products').insertOne(product);
+    const newProduct = new Product(product)
+    return newProduct.save()
+
+
+    // return db.collection('Products').insertOne(product);
 }
 export function deletePoduct(id) {
-    return db.collection('Products').deleteOne({ id: parseInt(id) || id });
+    return Product.findOneAndDelete({ id: ObjectId(id) })
+        // return db.collection('Products').deleteOne({ id: parseInt(id) || id });
+
 }
 export function updateProduct(id, newObj) {
-    return db.collection('Products').updateOne({ id: parseInt(id) || id }, { $set: newObj })
+    return Product.findOneAndUpdate({ id: ObjectId(id) }, newObj)
+        // return db.collection('Products').updateOne({ id: parseInt(id) || id }, { $set: newObj })
 }
--- express.mjs
+++ express.mjs
@@ -17,22 +17,30 @@ app.use(express.json());
 
 app.use(express.static('./build/'));
 const products = loadJson.sync('./products.json')
+
 app.get('/products', async(req, res) => {
-
-    res.send(await getProducts());
-
+    res.send(await getProducts(req.query));
 });
-// app.get('/products', (req, res) => {
 
-//     res.send(products);
 
-// });
-app.get('/products/:id', async(req, res) => {
-    res.send(await getProduct(req.params.id));
-})
 
 app.post('/products', async(req, res) => {
-    res.send(await addProduct(req.body));
+    try {
+        res.send(await addProduct(req.body));
+    } catch (err) {
+        console.log(err.message)
+        res.status(400)
+        res.send(err.message)
+    }
+})
+app.get('/products/:_id', async(req, res) => {
+    try {
+        res.send(await getProduct(req.params._id));
+    } catch (err) {
+        console.log(err.message)
+        res.status(400)
+        res.send(err.message)
+    }
 })
 app.delete('/products/:id', async(req, res) => {
     res.send(await deletePoduct(req.params.id))
